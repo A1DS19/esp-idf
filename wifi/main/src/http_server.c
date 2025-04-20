1782157881aa707f6613a29f730d7c9ccf579fb4
Jose Enrique Padilla <jp06171@gmail.com>
2025-04-19 23:14:54 -0600

    get ip info

--- wifi/main/src/http_server.c
+++ wifi/main/src/http_server.c
@@ -4,6 +4,7 @@
 #include <esp_log.h>
 #include <esp_ota_ops.h>
 #include <esp_timer.h>
+#include <esp_wifi.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -16,11 +17,14 @@
 
 #include "dht11.h"
 #include "esp_err.h"
+#include "esp_netif.h"
+#include "esp_netif_types.h"
 #include "esp_partition.h"
 #include "esp_system.h"
 #include "esp_wifi_types_generic.h"
 #include "freertos/idf_additions.h"
 #include "http_parser.h"
+#include "lwip/ip4_addr.h"
 #include "portmacro.h"
 
 // Firmware update status
@@ -423,6 +427,54 @@ static esp_err_t http_server_wifi_connect_status_json_handler(httpd_req_t *req)
 }
 
 /*
+ * wifiConnectInfo.json handler updates the web page with the connection
+ * information.
+ * @param req HTTP request for which the uri needs to be handled
+ * @return ESP_OK
+ */
+static esp_err_t http_server_wifi_connect_info_json_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "/wifiConnectInfo.json requested");
+    char ipInfoJSON[200];
+    memset(ipInfoJSON, 0, sizeof(ipInfoJSON));
+
+    char ip[IP4ADDR_STRLEN_MAX];
+    char netmask[IP4ADDR_STRLEN_MAX];
+    char gateway[IP4ADDR_STRLEN_MAX];
+
+    if (g_wifi_connect_status == HTTP_WIFI_STATUS_CONNECT_SUCCESS)
+    {
+        wifi_ap_record_t wifi_data;
+        ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&wifi_data));
+        char *ssid = (char *)wifi_data.ssid;
+
+        esp_netif_ip_info_t ip_info;
+        ESP_ERROR_CHECK(esp_netif_get_ip_info(esp_netif_sta, &ip_info));
+        esp_ip4addr_ntoa(&ip_info.ip, ip, IP4ADDR_STRLEN_MAX);
+        esp_ip4addr_ntoa(&ip_info.netmask, netmask, IP4ADDR_STRLEN_MAX);
+        esp_ip4addr_ntoa(&ip_info.gw, gateway, IP4ADDR_STRLEN_MAX);
+
+        snprintf(ipInfoJSON,
+                 sizeof(ipInfoJSON),
+                 "{\"ip\":\"%s\",\"netmask\":\"%s\",\"gw\":\"%s\",\"ap\":\"%s\"}",
+                 ip,
+                 netmask,
+                 gateway,
+                 ssid);
+    }
+    else
+    {
+        ESP_LOGE(TAG,
+                 "http_server_wifi_connect_info_json_handler: connect status not "
+                 "successfull");
+    }
+
+    httpd_resp_set_type(req, "application/json");
+    httpd_send(req, ipInfoJSON, strlen(ipInfoJSON));
+    return ESP_OK;
+}
+
+/*
  * Sets up the default httpd server configuration.
  * @return http server instance handle if sucessfull, NULL, otherwise.
  */
@@ -519,6 +571,12 @@ static httpd_handle_t http_server_configure(void)
                                                 .handler = http_server_wifi_connect_status_json_handler,
                                                 .user_ctx = NULL};
         httpd_register_uri_handler(http_server_handle, &wifi_connect_status_json);
+
+        httpd_uri_t wifi_connect_info_json = {.uri = "/wifiConnectInfo.json",
+                                              .method = HTTP_GET,
+                                              .handler = http_server_wifi_connect_info_json_handler,
+                                              .user_ctx = NULL};
+        httpd_register_uri_handler(http_server_handle, &wifi_connect_info_json);
 
         return http_server_handle;
     }
